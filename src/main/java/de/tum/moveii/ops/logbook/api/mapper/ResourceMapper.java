7a4633dac4adce8ff977789e8fe1903deb62e469
Alexandru Obada <alexandru.obad@gmail.com>
2017-02-01 00:44:19 +0100

    Cleaned code

--- src/main/java/de/tum/moveii/ops/logbook/api/mapper/ResourceMapper.java
+++ src/main/java/de/tum/moveii/ops/logbook/api/mapper/ResourceMapper.java
@@ -3,7 +3,7 @@ package de.tum.moveii.ops.logbook.api.mapper;
 /**
  * Created by Alexandru Obada on 29/01/17.
  */
-public interface ResourceMapper<M, R> {
+interface ResourceMapper<M, R> {
     R toResource(M message);
     M toMessage(R resource);
 }
